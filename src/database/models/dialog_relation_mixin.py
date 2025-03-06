f1824c8233f0fefa8f92a7d8d869406996ea257b
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-03-06 17:45:30 +0500

    06.03.2025 add votes database models

---
+++ src/database/models/dialog_relation_mixin.py
@@ -0,0 +1,33 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.database.models.dialog_model import DialogModel
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    declared_attr,
+    relationship
+)
+
+
+class DialogRelationMixin:
+    _dialog_id_nullable: bool = False
+    _dialog_id_unique: bool = False
+    _dialog_back_populates: str | None = None
+
+    @declared_attr
+    def dialog_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey("dialogs.id"),
+            unique=cls._dialog_id_unique,
+            nullable=cls._dialog_id_nullable
+        )
+
+    @declared_attr
+    def dialog(cls) -> Mapped["DialogModel"]:
+        return relationship(
+            "DialogModel",
+            back_populates=cls._dialog_back_populates
+        )
