5fae3d78875ae54155a4f90233374e3ec63a4537
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-03-06 16:56:38 +0500

    06.03.2025 add new document

--- scripts/add_embeddings_chroma.py
+++ scripts/add_embeddings_chroma.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from langchain_core.documents import Document
+import chromadb
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -11,6 +11,12 @@ BASE_DIR: Path = Path(__file__).resolve().parent.parent
 DOCUMENTS_DIR: Path = BASE_DIR / "documents"
 
 CHROMA_DIR = BASE_DIR / "chroma"
+
+
+client = chromadb.PersistentClient(path=str(CHROMA_DIR))
+client.delete_collection("dio-consult")
+
+print("---Коллекция dio-consult удалена---")
 
 
 directory = Path(DOCUMENTS_DIR)
@@ -44,7 +50,7 @@ print(f"Всего чанков: {len(chunks)}")
 print(f"Первые {N} чанков:")
 
 
-emdeddings = HuggingFaceEmbeddings(
+embeddings = HuggingFaceEmbeddings(
     model_name="ai-forever/sbert_large_nlu_ru",
     model_kwargs={"device": "cpu"},
     encode_kwargs={"normalize_embeddings": False}
@@ -54,6 +60,6 @@ emdeddings = HuggingFaceEmbeddings(
 vector_store = Chroma.from_documents(
     collection_name="dio-consult",
     documents=chunks,
-    embedding=emdeddings,
+    embedding=embeddings,
     persist_directory=str(CHROMA_DIR)
 )
