ab518b9dd1f1fa26837c199e87c7746f97e100a7
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-07-10 09:36:56 +0500

    10.07.2025 Add postgres and app building in docker compose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''import logging
+import logging
 import asyncio
 
 from aiogram import Bot
@@ -18,28 +18,4 @@ async def main() -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())'''
-
-
-import asyncio
-
-from uuid import uuid4
-
-from langgraph.graph.state import CompiledGraph
-
-from src.employee_assistant.dependencies import container
-from src.employee_assistant.ai_agent.utils import chat
-
-
-async def main() -> None:
-    thread_id = str(uuid4())
-    question = "На какую кнопку нужно нажать чтобы создать задание и где она находится?"
-
-    agent = await container.get(CompiledGraph)
-
-    text = await chat(thread_id=thread_id, content=question, agent=agent)
-
-    print(text)
-
-
-asyncio.run(main())
+    asyncio.run(main())
